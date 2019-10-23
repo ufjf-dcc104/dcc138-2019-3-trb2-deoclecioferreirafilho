167f5751d25023b4c84873ce4b0220a9473d5a2a
Deoclecio Filho <deoclecio@ice.ufjf.br>
2019-10-22 21:51:22 -0300

    Movimento ao personagem.

--- Sprite.js
+++ Sprite.js
@@ -22,9 +22,10 @@ function Sprite(params = {}) {
             8: { c: 1, l: 8, n: 8 }, // andando cima
 
         },
-        pose: 0,
+        pose: 3,
         speed: 0,
         frame: 0,
+        countAnim: 0,
         props: {},
         cooldown: 0,
         color: "blue",
@@ -57,27 +58,75 @@ Sprite.prototype.desenhar = function (ctx) {
 
 Sprite.prototype.mover = function (dt) {
     this.moverOrtogonal(dt)
-    this.frame += 2 * dt;
-    if (this.frame > this.poses[this.pose].n) { this.frame = 0 }
+    //if (this.frame > this.poses[this.pose].n) { this.frame = 0 }
     //this.frame += 6 * dt;
     //if (this.frame > 9) { this.frame = 0 }
 }
 
 Sprite.prototype.moverOrtogonal = function (dt) {
 
-    this.vx += this.speed;
-    this.vy += this.speed;
-
-    //this.a = this.a + this.va*dt;
-/*
+    /*
+    this.x += this.speed;
+    this.y += this.speed;
+    this.a = this.a + this.va*dt;
     this.vx = this.vx + this.ax * dt;//- this.vx * 0.9 * dt;
     this.vy = this.vy + this.ay * dt /*+ 120 * dt;
-
+    */
 
     this.mc = Math.floor(this.x / this.scene.map.SIZE);
     this.ml = Math.floor(this.y / this.scene.map.SIZE);
 
     this.aplicaRestricoes(dt);
     this.cooldown = this.cooldown - dt;
-    */
+}
+Sprite.prototype.aplicaRestricoes = function (dt) {
+
+    var dnx;
+    var dx;
+    dx = this.vx * dt;
+    dnx = dx;
+    dy = this.vy * dt;
+    dny = dy;
+    if (dx > 0 && this.scene.map.cells[this.mc + 1][this.ml].tipo != 0) {
+        dnx = this.scene.map.SIZE * (this.mc + 1) - (this.x + this.w / 2);
+        dx = Math.min(dnx, dx);
+    }
+    if (dx < 0 && this.scene.map.cells[this.mc - 1][this.ml].tipo != 0) {
+        dnx = this.scene.map.SIZE * (this.mc - 1 + 1) - (this.x - this.w / 2);
+        dx = Math.max(dnx, dx);
+    }
+    if (dy > 0 && this.scene.map.cells[this.mc][this.ml + 1].tipo != 0) {
+        dny = this.scene.map.SIZE * (this.ml + 1) - (this.y + this.h / 2);
+        dy = Math.min(dny, dy);
+    }
+    if (dy < 0 && this.scene.map.cells[this.mc][this.ml - 1].tipo != 0) {
+        dny = this.scene.map.SIZE * (this.ml - 1 + 1) - (this.y - this.h / 2);
+        dy = Math.max(dny, dy);
+    }
+    this.vy = dy / dt;
+    this.x = this.x + dx;
+    this.y = this.y + dy;
+
+    var MAXX = this.scene.map.SIZE * this.scene.map.COLUMNS - this.w / 2;
+    var MAXY = this.scene.map.SIZE * this.scene.map.LINES - this.h / 2;
+
+    if (this.x > MAXX) this.x = MAXX;
+    if (this.y > MAXY) {
+        this.y = MAXY;
+        this.vy = 0;
+    }
+    if (this.x - this.w / 2 < 0) this.x = 0 + this.w / 2;
+    if (this.y - this.h / 2 < 0) this.y = 0 + this.h / 2;
+
+}
+
+
+Sprite.prototype.colidiuCom = function (alvo) {
+    if (alvo.x + alvo.w / 2 < this.x - this.w / 2) return false;
+    if (alvo.x - alvo.w / 2 > this.x + this.w / 2) return false;
+
+    if (alvo.y + alvo.h / 2 < this.y - this.h / 2) return false;
+    if (alvo.y - alvo.h / 2 > this.y + this.h / 2) return false;
+
+    return true;
 }
